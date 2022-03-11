515705ee39bc2473da5b2421aa673b9a6dc42b04
Jiang Yu <2nth0nyj@gmail.com>
2022-03-11 13:51:52 +0800

    to problem 29, typescipt, shift dividend to the right so there is no overflow.

--- src/leetcode/29.ts
+++ src/leetcode/29.ts
@@ -14,39 +14,55 @@ export function divide(dividend: number, divisor: number): number {
     if (absoluteDivisor < 0) {
         absoluteDivisor = -absoluteDivisor
     }
-
-    while (abosoluteDividend >= absoluteDivisor) {
-        var shift: number = 0
-        var overflowed = false
-
-        var absoluteShiftDivisor = absoluteDivisor << shift
-        while (abosoluteDividend >= absoluteShiftDivisor) {
-            shift++
-            absoluteShiftDivisor = absoluteDivisor << shift
-            if (absoluteShiftDivisor < 0) {
-                overflowed = true
-                absoluteShiftDivisor = Math.pow(2, shift)
-            }
-            if (overflowed) {
-                absoluteShiftDivisor = Math.pow(2, shift)
-            }
+    var res = 0
+    var i = 1
+    var tmp = absoluteDivisor
+    while( abosoluteDividend >= absoluteDivisor) {
+        i = 1
+        tmp = absoluteDivisor
+        while (tmp <= (abosoluteDividend>>1)){
+            tmp <<= 1
+            i <<= 1
         }
-        if (shift > 0) {
-            quotient += (1 << (shift - 1))
-            abosoluteDividend -= absoluteDivisor << (shift - 1)
-        }
+        abosoluteDividend -= tmp
+        res += i
     }
+    if( !positive ) {
+        res = -res
+    }
+    return Math.min(Math.pow(2, 31) - 1, Math.max(res,  Math.pow(-2, 31)))
+}
+    // while (abosoluteDividend >= absoluteDivisor) {
+    //     var shift: number = 0
+    //     var overflowed = false
 
-    if (!positive) {
-        return -quotient
-    }
+    //     var absoluteShiftDivisor = absoluteDivisor << shift
+    //     while (abosoluteDividend >= absoluteShiftDivisor) {
+    //         shift++
+    //         absoluteShiftDivisor = absoluteDivisor << shift
+    //         if (absoluteShiftDivisor < 0) {
+    //             overflowed = true
+    //             absoluteShiftDivisor = Math.pow(2, shift)
+    //         }
+    //         if (overflowed) {
+    //             absoluteShiftDivisor = Math.pow(2, shift)
+    //         }
+    //     }
+    //     if (shift > 0) {
+    //         quotient += (1 << (shift - 1))
+    //         abosoluteDividend -= absoluteDivisor << (shift - 1)
+    //     }
+    // }
 
-    if (quotient >= (Math.pow(2, 31) - 1)) {
-        return Math.pow(2, 31) - 1
-    }
-    if (quotient <= Math.pow(-2, 31)) {
-        return Math.pow(-2, 31)
-    }
+    // if (!positive) {
+    //     return -quotient
+    // }
 
-    return quotient
-};
+    // if (quotient >= (Math.pow(2, 31) - 1)) {
+    //     return Math.pow(2, 31) - 1
+    // }
+    // if (quotient <= Math.pow(-2, 31)) {
+    //     return Math.pow(-2, 31)
+    // }
+
+    // return quotient
