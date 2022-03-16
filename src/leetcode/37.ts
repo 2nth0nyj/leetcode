1d5d282b689c2de1ff691a3bc8996c0def45fed2
Jiang Yu <2nth0nyj@gmail.com>
2022-03-16 09:29:03 +0800

    problem 37, typescript.

--- src/leetcode/37.ts
+++ src/leetcode/37.ts
@@ -1,54 +1,49 @@
 type Cell = {
-    row: number, column: number, current?: number, alternative?: number[]
+    row: number, column: number, current?: string, alternative?: string[]
 }
 
-function solveSudoku(board: string[][]): void {
+export function solveSudoku(board: string[][]): void {
     let stack: Cell[] = []
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            if (board[i][j] !== ".") {
+            if (board[i][j] === ".") {
                 stack.push({ row: i, column: j })
             }
         }
     }
+    printBoard(board)
+    var nextStack: number = 0
+    do {
+        nextStack = fillSudoku(stack, nextStack, board)
+    } while (nextStack < stack.length)
+    console.log( "after solved")
+    printBoard(board)
 };
 
-
-function fillSudoku(stack: Array<Cell>, board: string[][]) {
-    for (let s of stack) {
-        let n = cellAvailableNumbers(s.row, s.column, board)
+function fillSudoku(stack: Array<Cell>, index: number, board: string[][]): number {
+    var cell = stack[index] //this is current filling cell
+    var row = cell.row
+    var column = cell.column
+    var fillingArray: string[] = []
+    if (cell.alternative) {
+        fillingArray = cell.alternative
+    } else {
+        fillingArray = cellAvailableNumbers(row, column, board)
     }
-}
-
-function rowCheck(row: number, board: string[][]): boolean {
-    var s = new Set<string>()
-    let rowOk = board[row].every((v) => {
-        if (v == ".") {
-            return true
-        }
-        if (s.has(v)) {
-            return false
-        }
-        s.add(v)
-        return true
-    })
-    return rowOk
-}
-
-function columnCheck(column: number, board: string[][]) {
-    var s = new Set<string>()
-    let columnOk = board.every((row) => {
-        let c = row[column]
-        if (c == ".") {
-            return true
-        }
-        if (s.has(c)) {
-            return false
-        }
-        s.add(c)
-        return true
-    })
-    return columnOk
+    var nextIndex: number = 0
+    if (fillingArray.length == 0) {
+        board[row][column] = "."
+        delete cell.alternative
+        delete cell.current
+        nextIndex = index - 1
+    } else {
+        var c = fillingArray.shift() as string
+        board[row][column] = c
+        cell.current = c
+        cell.alternative = fillingArray
+        nextIndex = index + 1
+    }
+    return nextIndex
 }
 
 function cellAvailableNumbers(row: number, column: number, board: string[][]): string[] {
@@ -87,4 +82,42 @@ function rowColumnToGrid(row: number, column: number): Array<Array<number>> {
         result[1].push(addColumn)
     }
     return result
+}
+
+
+function printBoard(board: string[][]) {
+    board.forEach((row) => {
+        console.log(row.join(","))
+    })
+}
+
+function rowCheck(row: number, board: string[][]): boolean {
+    var s = new Set<string>()
+    let rowOk = board[row].every((v) => {
+        if (v == ".") {
+            return true
+        }
+        if (s.has(v)) {
+            return false
+        }
+        s.add(v)
+        return true
+    })
+    return rowOk
+}
+
+function columnCheck(column: number, board: string[][]) {
+    var s = new Set<string>()
+    let columnOk = board.every((row) => {
+        let c = row[column]
+        if (c == ".") {
+            return true
+        }
+        if (s.has(c)) {
+            return false
+        }
+        s.add(c)
+        return true
+    })
+    return columnOk
 }
--- src/main.ts
+++ src/main.ts
@@ -1,106 +1,109 @@
-import { ListNode } from './leetcode/listNode'
+// import { ListNode } from './leetcode/listNode'
 
-import { twoSum } from './leetcode/1'
-import { addTwoNumbers } from './leetcode/2'
-import { lengthOfLongestSubstring } from './leetcode/3'
-import { longestPalindrome } from './leetcode/5'
-import { convert } from './leetcode/6'
-import { reverse } from './leetcode/7'
-import { isMatch } from './leetcode/10'
-import { longestCommonPrefix } from './leetcode/14'
-import { threeSum } from './leetcode/15'
-import { letterCombinations } from './leetcode/17'
-import { fourSum } from './leetcode/18'
-import { isValid } from './leetcode/20'
-import { mergeKLists } from './leetcode/23'
-import { strStr } from './leetcode/28'
-import { divide } from './leetcode/29'
-import { findSubstring } from './leetcode/30'
-import { nextPermutation } from './leetcode/31'
-import { longestValidParentheses } from './leetcode/32'
-import { search } from './leetcode/33'
-import { searchRange } from './leetcode/34'
-import { searchInsert } from './leetcode/35'
-import { isValidSudoku } from './leetcode/36'
-
-
-
-let result1 = twoSum([1, 2, 3, 4, 45], 7)
-console.log("result 1= ", result1)
+// import { twoSum } from './leetcode/1'
+// import { addTwoNumbers } from './leetcode/2'
+// import { lengthOfLongestSubstring } from './leetcode/3'
+// import { longestPalindrome } from './leetcode/5'
+// import { convert } from './leetcode/6'
+// import { reverse } from './leetcode/7'
+// import { isMatch } from './leetcode/10'
+// import { longestCommonPrefix } from './leetcode/14'
+// import { threeSum } from './leetcode/15'
+// import { letterCombinations } from './leetcode/17'
+// import { fourSum } from './leetcode/18'
+// import { isValid } from './leetcode/20'
+// import { mergeKLists } from './leetcode/23'
+// import { strStr } from './leetcode/28'
+// import { divide } from './leetcode/29'
+// import { findSubstring } from './leetcode/30'
+// import { nextPermutation } from './leetcode/31'
+// import { longestValidParentheses } from './leetcode/32'
+// import { search } from './leetcode/33'
+// import { searchRange } from './leetcode/34'
+// import { searchInsert } from './leetcode/35'
+// import { isValidSudoku } from './leetcode/36'
+import { solveSudoku } from './leetcode/37'
 
 
-let result2 = addTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))), new ListNode(5, new ListNode(6, new ListNode(4))))
-// console.log( "result 2= ", result2)
 
-let result3 = lengthOfLongestSubstring("abcabcbb")
-console.log("result 3= ", result3)
+// let result1 = twoSum([1, 2, 3, 4, 45], 7)
+// console.log("result 1= ", result1)
 
 
-let result5 = longestPalindrome("cbbd")
-console.log("result5 = ", result5)
+// let result2 = addTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))), new ListNode(5, new ListNode(6, new ListNode(4))))
+// // console.log( "result 2= ", result2)
 
-let result6 = convert("PAYPALISHIRING", 4)
-console.log("result6 = ", result6)
-
-
-let result7 = reverse(123)
-console.log("result7 = ", result7)
-
-let result10 = isMatch("aa", "a*")
-console.log("result10 = ", result10)
-
-let result14 = longestCommonPrefix(["flower", "flow", "flight"])
-console.log("result14 = ", result14)
+// let result3 = lengthOfLongestSubstring("abcabcbb")
+// console.log("result 3= ", result3)
 
 
-let result15 = threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4])
-console.log("result15 = ", result15)
+// let result5 = longestPalindrome("cbbd")
+// console.log("result5 = ", result5)
 
-let result16 = letterCombinations("2343")
-console.log("result16 = ", result16)
-
-let result18 = fourSum([2, 2, 2, 2, 2], 8)
-console.log("result18 = ", result18)
-
-let result20 = isValid("()[]{}")
-console.log("result20 = ", result20)
-
-var listNodes = [
-    new ListNode(1, new ListNode(4, new ListNode(5))),
-    new ListNode(1, new ListNode(3, new ListNode(4))),
-    new ListNode(2, new ListNode(6)),
-]
-let result22 = mergeKLists(listNodes)
-console.log("result22 = ", result22)
+// let result6 = convert("PAYPALISHIRING", 4)
+// console.log("result6 = ", result6)
 
 
-let result28 = strStr("aaaaa", "bba")
-console.log("result28 = ", result28)
+// let result7 = reverse(123)
+// console.log("result7 = ", result7)
 
-let result29 = divide(-2147483648, -1)
-console.log("result29 = ", result29)
+// let result10 = isMatch("aa", "a*")
+// console.log("result10 = ", result10)
 
-// let result30 = findSubstring("bcabbcaabbccacacbabccacaababcbb",["c","b","a","c","a","a","a","b","c"])
-let result30 = findSubstring("wordgoodgoodgoodbestword", ["word", "good", "best", "good"])
-console.log("result30 = ", result30)
-
-var nums = [4, 5, 3, 2, 1]
-nextPermutation(nums)
-console.log("nums = ", nums)
+// let result14 = longestCommonPrefix(["flower", "flow", "flight"])
+// console.log("result14 = ", result14)
 
 
+// let result15 = threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4])
+// console.log("result15 = ", result15)
+
+// let result16 = letterCombinations("2343")
+// console.log("result16 = ", result16)
+
+// let result18 = fourSum([2, 2, 2, 2, 2], 8)
+// console.log("result18 = ", result18)
+
+// let result20 = isValid("()[]{}")
+// console.log("result20 = ", result20)
+
+// var listNodes = [
+//     new ListNode(1, new ListNode(4, new ListNode(5))),
+//     new ListNode(1, new ListNode(3, new ListNode(4))),
+//     new ListNode(2, new ListNode(6)),
+// ]
+// let result22 = mergeKLists(listNodes)
+// console.log("result22 = ", result22)
+
+
+// let result28 = strStr("aaaaa", "bba")
+// console.log("result28 = ", result28)
+
+// let result29 = divide(-2147483648, -1)
+// console.log("result29 = ", result29)
+
+// // let result30 = findSubstring("bcabbcaabbccacacbabccacaababcbb",["c","b","a","c","a","a","a","b","c"])
+// let result30 = findSubstring("wordgoodgoodgoodbestword", ["word", "good", "best", "good"])
+// console.log("result30 = ", result30)
+
+// var nums = [4, 5, 3, 2, 1]
+// nextPermutation(nums)
+// console.log("nums = ", nums)
+
+
+// // var result32 = longestValidParentheses("(()")
 // var result32 = longestValidParentheses("(()")
-var result32 = longestValidParentheses("(()")
-console.log("result32 = ", result32)
+// console.log("result32 = ", result32)
 
-var result33 = search([4, 5, 6, 7, 0, 1, 2], 0)
-console.log("result33 = ", result33)
+// var result33 = search([4, 5, 6, 7, 0, 1, 2], 0)
+// console.log("result33 = ", result33)
 
-var result34 = searchRange([5, 7, 7, 8, 8, 10], 8)
-console.log("result34 = ", result34)
+// var result34 = searchRange([5, 7, 7, 8, 8, 10], 8)
+// console.log("result34 = ", result34)
 
-var result35 = searchInsert([1, 3, 5, 6], 7)
-console.log("result35 = ", result35)
+// var result35 = searchInsert([1, 3, 5, 6], 7)
+// console.log("result35 = ", result35)
 
-var result36 = isValidSudoku([[".",".","4",".",".",".","6","3","."],[".",".",".",".",".",".",".",".","."],["5",".",".",".",".",".",".","9","."],[".",".",".","5","6",".",".",".","."],["4",".","3",".",".",".",".",".","1"],[".",".",".","7",".",".",".",".","."],[".",".",".","5",".",".",".",".","."],[".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".","."]])
-console.log( "result36 = ", result36)
+// var result36 = isValidSudoku([[".",".","4",".",".",".","6","3","."],[".",".",".",".",".",".",".",".","."],["5",".",".",".",".",".",".","9","."],[".",".",".","5","6",".",".",".","."],["4",".","3",".",".",".",".",".","1"],[".",".",".","7",".",".",".",".","."],[".",".",".","5",".",".",".",".","."],[".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".","."]])
+// console.log( "result36 = ", result36)
+
+solveSudoku([["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]])
